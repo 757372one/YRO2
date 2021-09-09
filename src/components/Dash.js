b37d11be9236994820005976d741ff881dc65bba
OAI <oai@757372im.local>
2021-09-09 03:14:17 -0600

    dash decimal fix

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -72,7 +72,7 @@ class Dash extends Component {
 	      });
 		  
 		   this.setState({
-     walletAddress: ((await (this.busdApp.methods.balanceOf(this.state.account).call()))/this.biggerNumber).toFixed(2)+"M"
+     walletAddress: ((await (this.busdApp.methods.balanceOf(this.state.account).call())))
 	      });
   }
   
@@ -105,7 +105,7 @@ class Dash extends Component {
 	      });
 		  
 		   this.setState({
-     walletAddress: ((await (this.busdApp.methods.balanceOf(wallet).call())))
+     walletAddress: ((await (this.busdApp.methods.balanceOf(wallet).call()))/this.biggerNumber).toFixed(6)+"M"
 	      });
     // document.getElementById('balanceBUSDRewards').innerHTML = 
     // ((await (this.busdApp.methods.getAccountDividendsInfo(wallet).call()))[4]/this.bigNumber).toFixed(2);
