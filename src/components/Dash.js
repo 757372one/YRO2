36a440dd93eeff04804677b83b13270deaaaedd1
V <v@Vs-MacBook-Pro.local>
2021-09-10 19:43:52 -0600

    total BUSD paid - fix

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -9,6 +9,7 @@ class Dash extends Component {
   YieldRocketContract = "0xd1087290aa03335b55cb9cc9ca59ed4d973ca949";
   biggerNumber =   "1000000000000000000";
   bigNumber =      "1000000000000000000";
+  totalnumber = "100000000000000000000000000000";
   balanceBUSDRewards = 0;
   totalBUSDRewards = 0;
   totalBUSDtokens = 0;
@@ -59,7 +60,7 @@ class Dash extends Component {
   async loadContract() {
     this.busdApp = new this.web3.eth.Contract(busdAbi, this.YieldRocketContract);
     let totalBUSD = await this.busdApp.methods.getTotalDividendsDistributed().call();
-    document.getElementById('totalBUSDRewards').innerHTML = (totalBUSD/this.bigNumber).toFixed(2);
+    document.getElementById('totalBUSDRewards').innerHTML = (totalBUSD/this.totalnumber).toFixed(2);
   }
 
   async connect() {
